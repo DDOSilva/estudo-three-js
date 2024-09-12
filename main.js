4f5b74145d2db05bbf4e59a733eb7931ee44ae15
DDOSilva <iol.davidias@gmail.com>
2024-09-12 19:54:21 -0300

    arena criada

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //importa a biblioteca do three.js, THREE é o objeto que contém toda a info da biblioteca
 import * as THREE from 'three';
+import { color } from 'three/webgpu';
 
 // cria uma cena onde podemos adicionar varias coisas
 const scene = new THREE.Scene(); 
@@ -13,17 +14,18 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
+scene.background = new THREE.Color (0xC0C0C0 );
+
 // cria um cubo e o adiciona a cena
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.CylinderGeometry( 8, 8, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+const arena = new THREE.Mesh( geometry, material );
+scene.add( arena );
 
 // ajusta a camera na posicao z
-camera.position.z = 5;
+camera.position.z = 20;
 
 // cria as variaveis a serem utilizadas
-let isRotating = true;
 const speed = 0.01;
 const moveSpeed = 0.1;
 
@@ -33,23 +35,23 @@ document.addEventListener('keydown', onDocumentKeyDown);
 // define o movimento do cubo a depender do evento 
 function onDocumentKeyDown(event) {
     switch (event.key) {
-        case '1':
-            isRotating = true; // ativa a rotação
-            break; 
-        case '0':
-            isRotating = false; // desativa a rotação
-            break;
         case 'w':
-            cube.position.z -= moveSpeed; // frente: eixo Z
+            arena.position.z -= moveSpeed; // frente: eixo Z
             break;
         case 's':
-            cube.position.z += moveSpeed; // trás: eixo Z
+            arena.position.z += moveSpeed; // trás: eixo Z
             break;
         case 'a':
-            cube.position.x -= moveSpeed; //esquerda: eixo X
+            arena.position.x -= moveSpeed; //esquerda: eixo X
             break;
         case 'd':
-            cube.position.x += moveSpeed; // direita: eixo X
+            arena.position.x += moveSpeed; // direita: eixo X
+            break;
+        case 'q':
+            arena.position.y += moveSpeed;
+            break;
+        case 'e':
+            arena.position.y -= moveSpeed;
             break;
     }
 }
@@ -57,10 +59,6 @@ function onDocumentKeyDown(event) {
 
 // loop que irá "animar" a cena
 function animate() {
-    if (isRotating) { // condição para rotação do cubo (nos eixos x e y)
-        cube.rotation.x += speed;
-        cube.rotation.y += speed;
-    }
 
     renderer.render( scene, camera ); // renderiza a cena a partir dos objetos 3d presentes em scene e da perspectiva de camera
 }
