f9111cb4534e53a464bc4392f946d9c624c7958b
DDOSilva <iol.davidias@gmail.com>
2024-09-15 20:42:04 -0300

    translated comments

--- main.js
+++ main.js
@@ -1,66 +1,63 @@
-//importa a biblioteca do three.js, THREE é o objeto que contém toda a info da biblioteca
+// import the three.js library, THREE is the object that contains all the library info
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { roughness } from 'three/webgpu'; // uma das propriedades do material utilizado na arena
+import { roughness } from 'three/webgpu'; // one of the properties of the material used in the arena
 
-// cria uma cena onde podemos adicionar varias "coisas"
+// creates scene and perspective camera
 const scene = new THREE.Scene(); 
-
-// cria uma camera com perspectiva
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 
-
-// cria e configura o renderizador que sera utilizado
+// creates and configures used renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-// altera a cor do plano de fundo
-scene.background = new THREE.Color ( 0xAEF7DD );
+// changes the background color
+scene.background = new THREE.Color ( 0x1FDE9A );
 
-// cria uma luz a cena (facilita a visualizacao)
+// creates a light in the scene (improves visibility)
 const light = new THREE.DirectionalLight(0xffffff, 4);
 light.position.set(15, 15, 15);
 scene.add(light);
 
-// variaveis que definem o tamanho da arena
+// variables that define the size of the arena
 const arenaRadius = 385;
 const arenaHeight = 11;
-// geometria da arena
-const arenaGeometry = new THREE.CylinderGeometry( arenaRadius, arenaRadius, arenaHeight, 96 ); // parametro 4: nivel de "detalhamento" da geometria
-const arenaMaterial = new THREE.MeshStandardMaterial( { // material da arena
+// geometry of the arena
+const arenaGeometry = new THREE.CylinderGeometry( arenaRadius, arenaRadius, arenaHeight, 96 ); // parameter 4: level of "detail" of the geometry
+const arenaMaterial = new THREE.MeshStandardMaterial( { // arena material
     color: 0x000000,
     roughness: 0.5
 });
 
-const arena = new THREE.Mesh( arenaGeometry, arenaMaterial ); // adiciona o material e a geometria a arena
-scene.add( arena ); // adiciona a arena a cena
+const arena = new THREE.Mesh( arenaGeometry, arenaMaterial ); // adds the material and geometry to the arena
+scene.add( arena ); // adds the arena to the scene
 
 const borderAdjust = 10;
 const borderGeometry = new THREE.CylinderGeometry( arenaRadius + borderAdjust, arenaRadius + borderAdjust, arenaHeight - 1, 96);
-const borderMaterial = new THREE.MeshStandardMaterial( { // material da borda branca ao redor da arena
+const borderMaterial = new THREE.MeshStandardMaterial( { // material of the white border around the arena
     color: 0xFFFFFF,
     roughness: 0.5
 });
 
-const border = new THREE.Mesh( borderGeometry, borderMaterial ); // adiciona o material e a geometria a borda
-scene.add( border ); // adiciona a borda a cena
+const border = new THREE.Mesh( borderGeometry, borderMaterial ); // adds the material and geometry to the border
+scene.add( border ); // adds the border to the scene
 
-// ajusta a camera na posicao z
+// adjusts the camera on the z position
 camera.position.z = 500;
 camera.position.y = 50;
 
-// cria as variaveis a serem utilizadas
+// creates the variables to be used
 const speed = 0.01;
 const moveSpeed = 0.1;           
-               
-// cria a orbita que permite mexer ao redor da cena com o mouse
+
+// creates the orbit that allows movement around the scene with the mouse
 orbit = new OrbitControls(camera, renderer.domElement); 
 
-// loop que irá "animar" a cena
+// loop that will "animate" the scene
 function animate() {
-    renderer.render( scene, camera ); // renderiza a cena a partir dos objetos 3d presentes em scene e da perspectiva de camera
+    renderer.render( scene, camera ); // renders the scene from the 3d objects present in scene and the perspective of camera
 }
 
 animate();
